49ade047043baedd21d671b96c369b2190f90acc
David Lee <david.lee.sl2634@yale.edu>
2020-05-17 20:14:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(string)
   new_element = string.split()
-  new_element.collect do |element|
-    p element.join(' ') << "."
-  end
+  new_element.collect { |element| element.capitalize }
 end

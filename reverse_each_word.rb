2be0f59748e3310103c13ce26f76d25da760cbee
David Lee <david.lee.sl2634@yale.edu>
2020-05-17 20:11:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   new_element = string.split()
   new_element.collect do |element|
-    p element.join("").reverse << . 
+    p element.join("").reverse << "." 
   end
 end
